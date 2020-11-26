4a040257deed180a73d5c71c4677cb4ce551c206
Pedro Alfonso <p.alfonsoag@gmail.com>
2020-11-25 22:05:39 -0400

    first version of the basic math module

---
+++ index.js
@@ -0,0 +1,45 @@
+module.exports = {
+  /**
+   * Addition of two numbers
+   *  @example
+   * n1 = 1 , n2 = 3 => result 4
+   * @param {*} n1 first number of the addition
+   * @param {*} n2 second number of the addition
+   */
+  add: function (n1, n2) {
+    return n1 + n2;
+  },
+
+  /**
+   * subtraction of two numbers
+   *  @example
+   * n1 = 1 , n2 = 3 => result -2
+   * @param {*} n1 first number of the subtraction
+   * @param {*} n2 second number of the subtraction
+   */
+  sub: function (n1, n2) {
+    return n1 - n2;
+  },
+
+  /**
+   * Multiplication of two numbers
+   *  @example
+   * n1 = 1 , n2 = 3 => result 3
+   * @param {*} n1 first number of the multiplication
+   * @param {*} n2 second number of the multiplication
+   */
+  mul: function (n1, n2) {
+    return n1 * n2;
+  },
+
+  /**
+   * Division of two numbers
+   *  @example
+   * n1 = 1 , n2 = 3 => result 0.33
+   * @param {*} n1 first number of the division
+   * @param {*} n2 second number of the division
+   */
+  div: function (n1, n2) {
+    return n1 / n2;
+  },
+};
---
+++ test.js
@@ -0,0 +1,9 @@
+const m = require(".");
+
+console.log(m.add(25, 26));
+
+console.log(m.sub(2020, 1995));
+
+console.log(m.mul(15, 15));
+
+console.log(m.div(933, 3));
